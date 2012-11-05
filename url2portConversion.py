a402aad3c9fe42d743422aecab7cb58a3fcaaf59
bigsnarfdude <ohprecio@gmail.com>
2012-11-05 12:38:42 -0500

    added ports to url conversion

--- url2portConversion.py
+++ url2portConversion.py
@@ -4,6 +4,7 @@ Program to parse URL's and obtain ports
 
 import socket
 from urlparse import urlparse
+from urlparse import urlunparse
 
 url_list = [    'http://www.example.com',
                 'https://www.example.com',
@@ -15,12 +16,22 @@ url_list = [    'http://www.example.com',
                 'pop3://pop.example.com',
                 'pop3s://pop.example.com',]
 
+ports_list = [ 80, 443, 21, 70, 25, 143, 993, 995 ]
+domain = 'example.com'
+
 def url_convert_port(host):
     parsed_url = urlparse(host)
     port = socket.getservbyname(parsed_url.scheme)
     print parsed_url, port
     return port
 
+def port_convert_url(port, domain):
+    return urlunparse((socket.getservbyport(port), domain,'/','','',''))
+
 print [ url_convert_port(host) for host in url_list ]
+print [ port_convert_url(port, domain) for port in ports_list ]
+
+
+
 
 
