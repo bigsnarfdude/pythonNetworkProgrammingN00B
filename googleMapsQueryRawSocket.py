dc70441a29a66ccee6a38b47e6025d83ae0f9d5e
bigsnarfdude <ohprecio@gmail.com>
2012-11-08 01:31:09 -0500

    simple socket get request google maps

---
+++ googleMapsQueryRawSocket.py
@@ -0,0 +1,11 @@
+import socket
+sock = socket.socket()
+sock.connect(('maps.google.com', 80))
+sock.sendall(
+'GET /maps/geo?q=207+N.+Defiance+St%2C+Archbold%2C+OH'
+'&output=json&oe=utf8&sensor=false HTTP/1.1\r\n''Host: maps.google.com:80\r\n'
+'User-Agent: search4.py\r\n'
+'Connection: close\r\n'
+'\r\n')
+rawreply = sock.recv(4096)
+print rawreply
