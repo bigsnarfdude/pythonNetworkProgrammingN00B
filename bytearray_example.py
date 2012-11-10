66835d980f86e746fa3113cdfba57ee02da9a9f3
bigsnarfdude <ohprecio@gmail.com>
2012-11-10 13:18:56 -0500

    read-write to bytearray for network

--- bytearray_example.py
+++ bytearray_example.py
@@ -1,6 +1,20 @@
 # making 8 bit words of data
 # bytearray is a mutable list of bytes
 
+# remaining = number of bytes being received (determined already)
+# write this in place of putting network data into a list
+# or appending to some type of list with +=
+# write to bytearray instead and enjoy the benefits of clean code
+
+
+msg = bytearray()
+while remaining > 0:
+        chunk = s.recv(remaining)    # Get available data
+            msg.extend(chunk)            # Add to message
+                remaining -= len(chunk) 
+
+
+
 file_input = u'٩(͡๏̯͡๏)۶'
 
 for c in file_input:
