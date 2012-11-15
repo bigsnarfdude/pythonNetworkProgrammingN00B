231af37f25d1016c38ea0eedf15ac3e858c88c2c
bigsnarfdude <ohprecio@gmail.com>
2012-11-15 15:06:10 -0500

    caching proxy select reactor

---
+++ server_proxy_cache_select.py
@@ -0,0 +1,54 @@
+import select
+import socket
+import sys
+import urllib2
+
+HOST = ''
+PORT = 8888
+backlog = 5
+size = 1024
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind ((HOST, PORT))
+server.listen(backlog)
+input = [server, sys.stdin]
+running = 1
+
+cache = {}
+
+def get_webpage(url):
+    result = urllib2.urlopen(url).read()
+    cache[url] = result
+    return result
+
+while running:
+    inputready, outputready, exceptready = select.select(input, [], []) 
+    
+    for s in inputready:
+
+        if s == server:
+            client, address = server.accept()
+            input.append(client)
+
+        elif s == sys.stdin:
+            junk = sys.stdin.readline()
+            running = 0
+
+        else:
+            data = s.recv(size)
+            if data:
+                if "http://" in data:
+                    url = data.strip()
+                    if url in cache:
+                        print "fetched from cache"
+                        data_to_client = cache[url]
+                        s.send(data_to_client)
+                    else:
+                        print "going to the interweb"
+                        data_to_client = get_webpage(url)
+                        s.send(data_to_client)
+            else:
+                s.close()
+                input.remove(s)
+
+server.close()
